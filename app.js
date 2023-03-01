0217af797d796b2648b70df6a46ea7965b660ff7
vitingr <vitorgabrielsbo1460@gmail.com>
2023-03-01 06:50:49 -0300

    Update para a Escola

--- app.js
+++ app.js
@@ -97,25 +97,8 @@ app.get("/", (req, res) => {
     res.render("naoLogado/cadastro")
 
 })
- 
-app.get("/login", (req, res) => {
 
-    const usuarioLogado = {
-        id: req.user_id,
-        nome: req.user.nome,
-        sobrenome: req.user.sobrenome,
-        nomeCompleto: req.user.nomeCompleto,
-        email: req.user.email,
-        senha: req.user.senha,
-        pais: req.user.pais,
-        estado: req.user.estado,
-        cidade: req.user.cidade,
-        escola: req.user.escola,
-        faculdade: req.user.faculdade,
-        foto: req.user.foto,
-        area: req.user.area,
-        seguidores: req.user.seguidores
-    }
+app.get("/login", (req, res) => {
 
     res.render("naoLogado/login")
 
@@ -217,13 +200,13 @@ app.post("/cadastrar", (req, res) => {
     }
 
     if (erros.length > 0) {
-        
+
         console.log(erros)
         res.render("naoLogado/cadastro", { erros: erros })
 
     } else {
 
-        Usuario.findOne({email: req.body.email}).lean().then((usuario) => {
+        Usuario.findOne({ email: req.body.email }).lean().then((usuario) => {
 
             if (usuario) {
 
--- routes/usuario.js
+++ routes/usuario.js
@@ -6,7 +6,24 @@ const mongoose = require("mongoose")
 
 router.get("/", (req, res) => {
 
-    res.render("usuario/inicio")
+    const usuarioLogado = {
+        id: req.user_id,
+        nome: req.user.nome,
+        sobrenome: req.user.sobrenome,
+        nomeCompleto: req.user.nomeCompleto,
+        email: req.user.email,
+        senha: req.user.senha,
+        pais: req.user.pais,
+        estado: req.user.estado,
+        cidade: req.user.cidade,
+        escola: req.user.escola,
+        faculdade: req.user.faculdade,
+        foto: req.user.foto,
+        area: req.user.area,
+        seguidores: req.user.seguidores
+    }
+
+    res.render("usuario/inicio", { usuario: usuarioLogado })
 
 })
 
