1027c0a3914a3b379b51e12eac7883131acf9dda
vitingr <vitorgabrielsbo1460@gmail.com>
2023-03-06 17:09:47 -0300

    Update Edit Profile

--- routes/usuario.js
+++ routes/usuario.js
@@ -19,6 +19,11 @@ const { infoUsuario } = require("../helpers/infoUsuario")
 
 // Funções
 
+function validarEmail(email) {
+    const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return regex.test(email)
+}
+
 // Páginas
 
 router.post("/publicar", (req, res) => {
