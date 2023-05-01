d1633d003c3cb42429111b22e47c8d9cc00f2830
vitingr <vitorgabrielsbo1460@gmail.com>
2023-05-01 14:15:29 -0300

    Update Salvamento Imagens

--- config/multer.js
+++ config/multer.js
@@ -5,20 +5,23 @@ function gerarLetras() {
     const letras = "abcdefghijklmnopqrstuvwxyz";
     let string = "";
     for (let i = 0; i < 3; i++) {
-      const randomIndex = Math.floor(Math.random() * letras.length);
-      string += letras[randomIndex];
+        const randomIndex = Math.floor(Math.random() * letras.length);
+        string += letras[randomIndex];
     }
     return string;
-  }
+}
 
-let string = gerarLetras()
+function gerarURL() {
+    return Date.now() + gerarLetras()
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "public/uploads/")
     },
     filename: function (req, file, cb) {
-        cb(null, Date.now() + string + path.extname(file.originalname))
+        const nomeArquivo = gerarURL()
+        cb(null, nomeArquivo + path.extname(file.originalname))
     }
 })
 
