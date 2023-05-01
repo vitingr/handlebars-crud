ec425b2a6014c1376ce427ad728dc29f993ec304
vitingr <vitorgabrielsbo1460@gmail.com>
2023-05-01 13:45:31 -0300

    Update Imagens

--- config/multer.js
+++ config/multer.js
@@ -1,12 +1,22 @@
 const multer = require("multer")
 const path = require("path")
 
+function numerosMulter() {
+    let numeros = [];
+    for (let i = 0; i < 3; i++) {
+      numeros.push(Math.floor(Math.random() * 10));
+    }
+    return numeros;
+}
+
+let numeros = numerosMulter()
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "public/uploads/")
     },
     filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname))
+        cb(null, Date.now() + numeros + path.extname(file.originalname))
     }
 })
 
