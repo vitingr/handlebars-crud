f4c1d6978506756bf36b4b479d5740dda327f1a6
vitingr <vitorgabrielsbo1460@gmail.com>
2023-05-01 00:22:10 -0300

    Update Database

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV == "production") {
 
-    module.exports = {mongoURI: "mongodb://mongo:InZx7piilfoDRxyG2Goh@containers-us-west-71.railway.app:7575"}
+    module.exports = {mongoURI: "mongodb://mongo:WFZsT9K2BqFPpVAD0eiR@containers-us-west-52.railway.app:7297"}
 
 } else {
 
